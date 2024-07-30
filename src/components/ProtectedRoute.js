37b546708e09b460aebd3cb00a5d441bf2fb75c6
Stevebeans <95848156+Stevebeans@users.noreply.github.com>
2024-07-29 22:39:49 -0400

    test

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,7 +2,7 @@
 
 import { useContext, useEffect } from "react";
 import { useRouter } from "next/router";
-import AuthContext from "@/components/utils/AuthContext";
+import AuthContext from "@/utils/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
